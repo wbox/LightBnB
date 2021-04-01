98a5fc9ec88f404fae3917563ee420a530d90ace
wbox <sribas@gmail.com>
2021-03-31 21:09:21 -0400

    Add db folder and connection.js

---
+++ LightBnB_WebApp-master/server/db/connection.js
@@ -0,0 +1,16 @@
+const pg = require('pg');
+
+const Client = pg.Client;
+
+const configDb ={
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT
+};
+
+const client = new Client(configDb);
+client.connect();
+
+module.exports = { client };
