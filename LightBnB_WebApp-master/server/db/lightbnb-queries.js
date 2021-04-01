e25cd0efd2c17ab423860a08eeea9b393c4e9b77
wbox <sribas@gmail.com>
2021-03-31 21:19:32 -0400

    Add getAllProperties function

---
+++ LightBnB_WebApp-master/server/db/lightbnb-queries.js
@@ -0,0 +1,8 @@
+const client = require('./connection');
+
+const getAllProperties = (limit, cb) => {
+  client
+    .query('SELECT * FROM properties LIMIT ' + limit + ';')
+    .then(res => console.log(res.rows))
+    .catch(err => console.error(err.stack))    
+};  
